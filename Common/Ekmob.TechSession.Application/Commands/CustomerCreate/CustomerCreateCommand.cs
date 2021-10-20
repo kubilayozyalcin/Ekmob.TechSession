d79b1a2da851c86ba37db58b1b7b3bf40bcdc507
Kubilay Özyalçın <kubilay.ozyalcin@ekmob.com>
2021-10-20 16:58:47 +0300

    fix model

--- Common/Ekmob.TechSession.Application/Commands/CustomerCreate/CustomerCreateCommand.cs
+++ Common/Ekmob.TechSession.Application/Commands/CustomerCreate/CustomerCreateCommand.cs
@@ -5,10 +5,10 @@ namespace Ekmob.TechSession.Application.Commands.CustomerCreate
 {
     public class CustomerCreateCommand : IRequest<CustomerResponse>
     {
-        public string Id { get; set; }
         public string Name { get; set; }
         public string IdentityNumber { get; set; }
         public string Email { get; set; }
+        public string DepartmentId { get; set; }
         public string DepartmentName { get; set; }
         public string JobTitle { get; set; }
 
